c1f9bb7a581d91565cfaa391b3c33274b4e16ca9
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2025-04-05 23:30:45 +0600

    some

--- src/Components/RegisterUserUpdate.jsx
+++ src/Components/RegisterUserUpdate.jsx
@@ -67,7 +67,7 @@ const RegisterUserUpdate = () => {
    
     return (
         <div>
-            <h1 className="text-3xl font-bold text-center my-9">Register User Update</h1>
+           
             
             <h1 className="text-3xl font-bold text-center my-9">Register User Update</h1>
 
